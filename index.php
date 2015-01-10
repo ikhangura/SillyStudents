f8598cf34ec8e410c26d18e4cbbc41aafc7b951c
ikhangura <ikhangura@gmail.com>
2015-01-09 18:12:34 -0800

    new setup

--- index.php
+++ index.php
@@ -10,9 +10,50 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php
-        echo "Hello Out there  ... I live";
-        echo " I new line added ";
-        ?>
+       <?php
+       
+    include('Student.php');
+    /* First Student infomation */
+    
+        $first = new Student();
+        $first->surname = "Doe";
+        $first->first_name = "John";
+        $first->add_email('home','john@doe.com');
+        $first->add_email('work','jdoe@mcdonalds.com');
+        $first->add_grade(65);
+        $first->add_grade(75);
+        $first->add_grade(55);
+    /* Second Student infomation */
+        $second = new Student();
+        $second->surname = "Einstein";
+        $second->first_name = "Albert";
+        $second->add_email('home','albert@braniacs.com');
+        $second->add_email('work1','a_einstein@bcit.ca');
+        $second->add_email('work2','albert@physics.mit.edu');
+        $second->add_grade(95);
+        $second->add_grade(80);
+        $second->add_grade(50);
+    /* Third Student infomation */    
+        $third = new Student();
+        $third->surname = "Khnagura";
+        $third->first_name = "Inderjeet";        
+        $third->add_email('work','ikhangura@bcit.ca');
+        $third->add_grade(69);
+        $third->add_grade(80);
+        $third->add_grade(82);
+      
+        $students = array();
+        $students['j123'] = $first; 
+        $students['a456'] = $second;
+        $students['a222'] = $third;
+        
+        // sorting the output
+        ksort($students);
+        
+        // display all students on screen 
+        foreach($students as $student)
+        echo $student->toString();
+    
+?> 
     </body>
 </html>
