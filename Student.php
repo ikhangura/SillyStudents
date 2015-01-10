0536a8f9dcc0ea94d584ca062580ab6e7a32ec01
ikhangura <ikhangura@gmail.com>
2015-01-09 18:12:23 -0800

    new setup

--- Student.php
+++ Student.php
@@ -20,26 +20,32 @@ class Student {
         $this->emails = array();
         $this->grades = array();
     }
+     /* Funation to add email address */
     function add_email($which,$address) {
     $this->emails[$which] = $address;
     }
-
+    /* Funation to add grades */
     function add_grade($grade) {
     $this->grades[] = $grade;
     } 
+    /* Get the Avarge of grades */
+    
     function average() {
     $total = 0;
     foreach ($this->grades as $value)
     $total += $value;
     return $total / count($this->grades);
     } 
+   /* Format the output : Primary email on first line  */ 
+    
     function toString() {
     $result = $this->first_name . ' ' . $this->surname;
-    $result .= ' ('.$this->average().")\n";
+    $result .= ' ('.$this->average().") ";
     foreach($this->emails as $which=>$what)
     
-    $result .= $which . ': '. $what. "\n";
+    $result .= $which . ': '. $what. "\n ";
     $result .= "\n";
     return '<pre>'.$result.'</pre>';
     } 
 }
+
